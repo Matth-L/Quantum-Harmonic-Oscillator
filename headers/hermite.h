655b580a2ee7efac705d176d15a13dab554cf84c
Matthias Lapu <matthias.lapu@ensiie.fr>
2023-09-27 11:53:02 +0200

    Ajout : Polynôme d'hermite V1

--- headers/hermite.h
+++ headers/hermite.h
@@ -1,10 +1,37 @@
-class Hermite 
-{
-    private:
-        int num;
-    public: 
-        // the class should contain the number AND the unit ???  
-        Hermite(int num, float unit);
-        int hermitianPolynome();
+/**
+ * @file hermite.h
+ */
 
+#ifndef HERMITE_H
+#define HERMITE_H
+#include <armadillo>
+#include <iostream>
+
+/**
+ * @brief Hermite's object containing :
+ * - the value of n (int)
+ * - an array of all the formulas pre-computed (armadillo matrix)
+ */
+class Hermite {
+public:
+  /**
+   * the n & z value given by the user
+   */
+  int n;
+  float z;
+
+  /**
+   * @brief this class contains the initial value of N and Z
+   */
+  Hermite(unsigned int, float);
+
+  /**
+   * @brief compute the value of the hermite's polynome
+   *
+   * @param n
+   * @param z
+   */
+  float polynomeHermite(unsigned int, float);
 };
+
+#endif // HERMITE_H
---
+++ src/main.cxx
@@ -0,0 +1,6 @@
+#include "../headers/hermite.h"
+
+int main() {
+  Hermite a = Hermite(4, 1);
+  return 0;
+}
